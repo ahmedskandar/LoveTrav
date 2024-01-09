858766947c6a8d36ce036496bd775ae428961711
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-01-09 23:47:18 +0300

    feat: Implement next ui router to work with next js

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
   return <NextUIProvider>{children}</NextUIProvider>;
 }
