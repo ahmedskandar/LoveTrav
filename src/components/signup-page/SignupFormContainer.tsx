7a6e2db759973955931deb64db516a9ec24ca396
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-01-10 23:58:50 +0300

    feat: Implement sign up container

---
+++ src/components/signup-page/SignupFormContainer.tsx
@@ -0,0 +1,17 @@
+import Logo from "@/components/ui/logo";
+import Heading from "../ui/heading";
+import SignupForm from "./SignupForm";
+
+const SignupFormContainer = () => {
+  return (
+    <div className="flex basis-1/2 flex-col items-center p-10 md:h-[100svh] md:overflow-y-scroll">
+      <Logo />
+      <div className="mx-auto w-full max-w-md">
+        <Heading className="mt-8">Sign Up</Heading>
+        <SignupForm />
+      </div>
+    </div>
+  );
+};
+
+export default SignupFormContainer;
