3f744fb02c77a1ac2a34c788793ef552e3f899a4
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-01-11 17:19:45 +0300

    chore: Add image message initial state for sign up

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,10 +2,11 @@ export const supabaseKey = process.env.NEXT_SUPABASE_API_KEY!;
 export const rapidApiKey = process.env.NEXT_RAPID_API_KEY!;
 
 export const initialSignUpFormState = {
-   usernameMessage: "",
-   passwordMessage: "",
-   confirmPasswordMessage: "",
-   emailMessage: "",
-   nationalitySelectMessage: "",
-   dbMessage: "",
- };
+  usernameMessage: "",
+  passwordMessage: "",
+  confirmPasswordMessage: "",
+  emailMessage: "",
+  nationalitySelectMessage: "",
+  imageMessage: "",
+  dbMessage: "",
+};
