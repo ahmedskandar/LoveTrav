70b4ea649975c47b8e7b978e94d441351d2fd228
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-01-12 15:45:42 +0300

    feat: Implement supabase server component connection

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,5 +1,25 @@
-import { supabaseKey } from '@/lib/constants';
-import {createBrowserClient} from '@supabase/ssr'
+import { supabaseKey } from "@/lib/constants";
+import { createBrowserClient, createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
 
 export const supabaseUrl = "https://dzypyxpbrupmlvqsxdmw.supabase.co";
-export const supabase = createBrowserClient(supabaseUrl, supabaseKey)
+
+export const supabaseClient = createBrowserClient(supabaseUrl, supabaseKey);
+
+//For server actions and route handlers
+export const supabaseServer = createServerClient(supabaseUrl, supabaseKey, {
+  cookies: {
+    get(name) {
+      //Return cookie with the name 'name' here
+      return cookies().get(name)?.value;
+    },
+    set(name, value, options) {
+      //Set the cookies
+      cookies().set(name, value, options);
+    },
+    remove(name, options) {
+      //Delete the cookie
+      cookies().set(name, "", options);
+    },
+  },
+});
