bda5fb3b84f65bc48e63a3a28a7021c5b11c1bf4
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-01-12 15:38:48 +0300

    chore: Configured sonner

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,8 +2,14 @@
 
 import { NextUIProvider } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
+import { Toaster } from "sonner";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
-  return <NextUIProvider navigate={router.push}>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider navigate={router.push}>
+      <Toaster position="top-center" richColors />
+      {children}
+    </NextUIProvider>
+  );
 }
