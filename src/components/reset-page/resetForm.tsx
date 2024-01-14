12708f20bfa56b125d7f6e7b1970b5a1607ea0c5
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-01-14 18:44:11 +0300

    fix: Redirect path after resetitng form

--- src/components/reset-page/resetForm.tsx
+++ src/components/reset-page/resetForm.tsx
@@ -24,7 +24,7 @@ const ResetForm = () => {
     }
     if (formState.resetSuccess) {
       toast.success(formState.resetSuccess);
-      redirect("/");
+      redirect("/login");
     }
   }, [formState]);
 
